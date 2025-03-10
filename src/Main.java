9e1df3cb1bbd494ceb2d5949716d9e04f8f768d7
mat-thedev <ra4649@feitep.edu.br>
2025-03-10 20:17:48 -0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        String message= "Hello there";
+        String world = "Universe";
+        world = world.toLowerCase();
+        System.out.println(world.contains("u"));
+    }
+}
