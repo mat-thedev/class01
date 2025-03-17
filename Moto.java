c22ac65700adb07f082ae31bee8b7d9f5ad4023b
raphael44-44 <ra3865@feitep.edu.br>
2025-03-17 20:30:49 -0300

    Update Moto.java

--- Moto.java
+++ Moto.java
@@ -7,6 +7,37 @@ public class Moto extends Automovel{
     super(marca, anoDeFabricacao, modelo);
   }
 
+  public Moto(String tipo, String motor, Boolean partidaEletrica) {
+    this.tipo = tipo;
+    this.motor = motor;
+    this.partidaEletrica = partidaEletrica;
+  }
+
+  public String getTipo() {
+    return tipo;
+  }
+
+  public void setTipo(String tipo) {
+    this.tipo = tipo;
+  }
+
+  public String getMotor() {
+    return motor;
+  }
+
+  public void setMotor(String motor) {
+    this.motor = motor;
+  }
+
+  public Boolean getPartidaEletrica() {
+    return partidaEletrica;
+  }
+
+  public void setPartidaEletrica(Boolean partidaEletrica) {
+    this.partidaEletrica = partidaEletrica;
+  }
+
   public void abrirValvulaCombustivel(){};
 
 }
+
